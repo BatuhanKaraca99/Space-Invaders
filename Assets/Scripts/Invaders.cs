d475054a16a999a564914160039d3682bf1319ce
Batuhan KARACA <b.karaca.1999@gmail.com>
2023-05-31 13:08:12 +0300

    Killing Invaders
    AnimationCurve

--- Assets/Scripts/Invaders.cs
+++ Assets/Scripts/Invaders.cs
@@ -5,7 +5,11 @@ public class Invaders : MonoBehaviour
     public Invader[] prefabs;
     public int rows = 5;
     public int columns = 11;
-    public float speed = 1.0f;
+    public AnimationCurve speed; //x,y graph according to percent
+
+    public int amountKilled { get; private set; } //how many kills
+    public int totalInvaders => this.rows * this.columns; //calculated property
+    public float percentKilled => (float)this.amountKilled / (float)this.totalInvaders;
 
     private Vector3 _direction = Vector3.right; //initially right
 
@@ -22,6 +26,7 @@ public class Invaders : MonoBehaviour
             for(int col = 0; col < this.columns; col++)
             {
                 Invader invader = Instantiate(this.prefabs[row], this.transform);
+                invader.killed += InvaderKilled;
                 Vector3 position = rowPosition;
                 position.x += col * 2.0f; //column+spacing
                 invader.transform.localPosition = position; //local position
@@ -31,7 +36,7 @@ public class Invaders : MonoBehaviour
 
     private void Update()
     {
-        this.transform.position += _direction * this.speed * Time.deltaTime;
+        this.transform.position += _direction * this.speed.Evaluate(this.percentKilled) * Time.deltaTime;
 
         Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
         Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
@@ -61,5 +66,10 @@ public class Invaders : MonoBehaviour
         Vector3 position = this.transform.position;
         position.y -= 1.0f;
         this.transform.position = position;
+    }
+
+    private void InvaderKilled()
+    {
+        this.amountKilled++;
     }
 }
